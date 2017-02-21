a8e7a47f00976d2af14aef455b9e896337efc4b3
slackjockey <this.is.salvador.ortiz@gmail.com>
2017-02-21 11:31:13 -0600

    caught up test.js

--- test.js
+++ test.js
@@ -3,13 +3,14 @@ var Async = require(path.join(__dirname, 'lib', 'master'));
 
 Async.shared.set('testValue', Math.round(Math.random() * 10e15));
 Async.shared.set('helloWorld', function(){
-  console.log(this.get('testValue'));
+  console.log('called!');
+  return 667;
 });
 
 
-Async.execute('helloWorld');
-Async.execute('helloWorld');
+Async.execute('helloWorld').then(function(res){ console.log('returned!\t', JSON.stringify(res)); });
 
 Async.execute(function(){
-  console.log(this);
-});
+  console.log('called!');
+  return 666;
+}).then(function(res){ console.log('returned!\t', JSON.stringify(res)); });
